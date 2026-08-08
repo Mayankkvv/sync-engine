0fcba6d3ed70e6f169e66c8408874a7ca1b3400a
Mayankkvv <mayankkumar2325@gmail.com>
2026-08-08 11:23:25 +0530

    Connect backend to MongoDB with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+//http://localhost:${PORT}
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Sync Engine backend is running" });
